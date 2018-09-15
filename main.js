8fe486aa709bff4d444b573d5a6feb66bd17da5c
Adam Grabowski <13grabek@wp.pl>
2018-09-15 11:24:52 +0200

    zadanie23

---
+++ main.js
@@ -0,0 +1,10 @@
+var name = "Adam";
+var surname = "Grabowski";
+window.onload = function() {
+            
+                document.getElementById('demo').innerHTML = name + " " + surname;
+                console.log("asa");
+           
+}
+
+
